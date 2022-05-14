aebc40143886d0b16c9d49110f11140b60c05c30
kimhyeonjin <khj745700@naver.com>
2022-05-14 17:11:06 +0900

    Add MemberLogin 구현

--- src/main/java/com/kit/cgv/jpa/domain/member/MemberRepository.java
+++ src/main/java/com/kit/cgv/jpa/domain/member/MemberRepository.java
@@ -4,4 +4,5 @@ import com.kit.cgv.jpa.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByMemberLoginID(String memberLoginId);
 }
