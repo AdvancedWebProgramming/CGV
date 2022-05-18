b468175393c0ac8f261a8d6a6de9d6d48b260415
kimhyeonjin <khj745700@naver.com>
2022-05-18 13:54:05 +0900

    Docs Backend config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, {useState, useEffect} from 'react';
 function App() {
+  /*
+  useEffect(() => {
+    fetch("/hello")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setMessage(data);
+      });
+  },[]);
+   */
   return (
     <div className="App">
       <header className="App-header">
